a026fc85e69253b06d27cc0f7d281f5a3454a040
Matthieu Levesque <matthieu.levesque@gmail.com>
2016-03-01 01:10:21 -0500

    Service migration start.

---
+++ py_shrt_lkr/core/services/link.py
@@ -0,0 +1,78 @@
+import transaction
+
+import sqlalchemy
+
+from sqlalchemy.orm.exc import NoResultFound
+
+from ..models.links import (
+	Link,
+	LinkHit,
+)
+
+from ..models.taxonomy import (
+	Tag
+)
+
+from py_shrt_lkr.helpers import (
+	list_diff
+)
+
+class LinkService(object):
+	def __init__(self, dbsession):
+		self.dbsession = dbsession
+	def create_link(self, url, name=None, title=None, description=None):
+		link = Link(url=url, name=name, title=title, description=description)
+
+		with transaction.manager:
+			self.dbsession.add(link)
+			created_id = self.dbsession.execute(sqlalchemy.func.max(Link.id)).first()[0]
+
+	def edit_link(self, id, name=None, title=None, description=None, shorty = None, url = None, tags=None):
+		link = self.get_link_by_id(id)
+
+		with transaction.manager:
+			link.title = title
+			link.description = description
+			link.shorty = shorty
+			link.url = url
+
+			currentTagLst = list(map(lambda x: x.name, link.tags))
+
+			newTags = list_diff(currentTagLst, formTags)
+			deletedTags = list_diff(formTags, currentTagLst)
+
+			#Delete the removed tags
+			if len(deletedTags)>0:
+				delIndx=list(map(lambda x: currentTagLst.index(x), deletedTags))
+				delIndx.sort()
+				delIndx.reverse()
+				print("Del indx "+str(delIndx))
+				for indx in delIndx:
+					print("Remove indx : %d"%indx)
+					link.tags.pop(indx)
+
+			#Insert the new tags
+			if len(newTags)>0:
+				for tagName in newTags:
+					tag = None
+					try:
+						tag=self.dbsession.query(Tag).filter_by(name=tagName).one()
+					except NoResultFound:
+						#If not found we create it
+						tag=Tag(name=tagName)
+					link.tags.append(tag)
+
+	def get_link_by_id(self, id):
+		try:
+			return self.dbsession.query(Link).filter_by(id=id).one()
+		except NoResultFound:
+			return None;
+
+	def delete_link(self, id):
+		if id != None:
+			link = self.get_link_by_id(id)
+
+			if link != None:
+				self.dbsession.delete(link)
+				transaction.commit()
+
